ea58f44bdb6d1168ee8855191088add4a27cf9ae
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-04-26 16:35:59 +0200

    finish todo in db controller

--- lib/php/dbcontroller.php
+++ lib/php/dbcontroller.php
@@ -2,6 +2,25 @@
     class DBController {
 
         /**
+         * ? functions defined on this class :
+         * *   public static function getController() 
+         *        Gets the database controller 
+         * *   public function getPokemonById( $id )
+         *        Gets pokemon by id 
+         * *   public function getUserByUsername( $username )
+         *        Gets user by username 
+         * *   public function getMossaById( $id )
+         *        Gets mossa by id 
+         * *   public function getPokemonList()
+         *        Gets all the pokemons in the database
+         * *   public function getMosseList()
+         *        Gets all the mosse in the database
+         * *   public function getMosseListByPokemon( $id )
+         *        Gets all the mosse that a pokemon can learn
+         */
+
+
+        /**
          * * local db values 
          */ 
         private static $servername = "localhost";
@@ -19,13 +38,13 @@
         private static $controller = null; 
         private $connection;
 
-        function __construct(){
+        private function __construct(){
     
             $this->connection = new mysqli(self::$servername, self::$username, self::$password, self::$database);
             if ($this->connection->connect_error) {
                 die("Connection failed: " . $this->connection->connect_error);
             }
-            echo "Connected successfully \n";
+            // echo "Connected successfully \n";
         }
 
         public static function getController() {
@@ -35,56 +54,150 @@
        
           return self::$controller;
         }
-
-        // ideally this might get removed
-        public function getConnection(){
-            return $this->connection;
-        }
         
-        // ideally this might get removed
-        public function query($query){
-            return $this->connection.query();
+        private function query($query){
+            $obj = Null;
+            if( $result = $this->connection->query($query) ){
+                $obj = $result -> fetch_all();
+                if (sizeof($obj)==0){
+                    $obj = Null;
+                }
+                $result->free_result(); 
+            }
+            return $obj;
         }
 
-        public function getPokemonById( $id ){
-            // TODO
-            $this->connection.query();
-        }
-        
+        /**
+         * Gets user by username 
+         * @param string $username String that identifies the user
+         * @return  array(   string => username, 
+         *                  string => password, 
+         *               )
+         *          or null if no match is found
+         */
         public function getUserByUsername( $username ){
-            // TODO
-            $this->connection.query();
-        }
-    
-        public function getMossaById( $id ){
-            // TODO
-            $this->connection.query();
+            $query = "select * from utente where username = ".$username;
+            return $this->query($query);
         }
 
-        public function getPokemonList(){
-            // TODO
-            $this->connection.query();
+        /**
+         * Gets pokemon by id 
+         * @param int $id Integer that identifies the pokemon
+         * @return  array(   string => id = int, 
+         *                   string => nome, 
+         *                   string => tipo1,
+         *                   string => tipo2 == Null if not exists ,
+         *                   string => ps = int,
+         *                   string => att = int,
+         *                   string => dif = int,
+         *                   string => attsp = int,
+         *                   string => difsp = int,
+         *                   string => uber = int[0,1]
+         *               )
+         *          or null if no match is found
+         */
+        public function getPokemonById( $id ){
+            $query =  "select * from pokemon where pokemon.id = ".strval($id);
+            $result = $this->query($query);
+            if($result != Null)
+                return $result[0];
+            else 
+                return $result;
         }
-
-        public function getMosseList(){
-            // TODO
-            $this->connection.query();
-        }
-
         
+        
+        /**
+         * Gets mossa by id 
+         * @param int $id Integer that identifies the mossa
+         * @return  array(
+         *                   string => id = int, 
+         *                   string => nome, 
+         *                   string => tipo,
+         *                   string => categoria,
+         *                   string => potenza = int,
+         *                   string => precisione = int,
+         *                   string => descrizione
+         *               )
+         *          or null if no match is found
+         */
+        public function getMossaById( $id ){
+            $query = "select * from mossa where id =".strval($id);
+            $result = $this->query($query);
+            if($result != Null)
+                return $result[0];
+            else 
+                return $result;
+        }
+
+
+        /**
+         * Gets all the pokemons in the database
+         * @return  array(
+         *              array(
+         *                   string => id = int, 
+         *                   string => nome, 
+         *                   string => tipo1,
+         *                   string => tipo2,
+         *                   string => ps = int,
+         *                   string => att = int,
+         *                   string => dif = int,
+         *                   string => attsp = int,
+         *                   string => difsp = int,
+         *                   string => uber = int[0,1]
+         *                  )
+         *               )
+         *          or null if no match is found
+         */
+        public function getPokemonList(){
+            $query = "select * from pokemon";
+            return $this->query($query);
+        }
+
+        /**
+         * Gets all the mosse in the database
+         * @return  array(   
+         *              array(
+         *                   string => id = int, 
+         *                   string => nome, 
+         *                   string => tipo,
+         *                   string => categoria,
+         *                   string => potenza = int,
+         *                   string => precisione = int,
+         *                   string => descrizione
+         *                   )
+         *               )
+         *          or null if no match is found
+         */
+        public function getMosseList(){
+            $query = "select * from mossa";
+            return $this->query($query);
+        }
+
+        /**
+         * Gets all the mosse that a pokemon can learn
+         * @param int $id Integer that identifies the pokemon
+         * @return  array(   
+         *              array(
+         *                   string => id = int, 
+         *                   string => nome, 
+         *                   string => tipo,
+         *                   string => categoria,
+         *                   string => potenza = int,
+         *                   string => precisione = int,
+         *                   string => descrizione
+         *                   )
+         *               )
+         *          or null if no match is found
+         */        
         public function getMosseListByPokemon( $id ){
-            // TODO
-            $this->connection.query();
+            $query = "select mossa.*
+            from mossa,pokemon,impara 
+            where impara.pokemon = pokemon.id 
+                and mossa.id = impara.mossa 
+                and pokemon.id = ".strval($id);
+            return $this->query($query);
         }
 
     }
 
-
-    /**
-     * experiment 
-     */
-    echo "trying to connect  \n";
-    $obj = DBController::getController();
-    //var_dump($obj);
-    
 ?>
