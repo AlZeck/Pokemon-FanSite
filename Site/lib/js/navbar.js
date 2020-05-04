50d317ff2ac54b4588d985485104ddc247a61165
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-05-05 01:11:06 +0200

    navbar update + script to auto add

---
+++ Site/lib/js/navbar.js
@@ -0,0 +1,13 @@
+function includeNavBar() {
+  fetch("./navbar.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      var curr = document.querySelector("body").innerHTML 
+      document.querySelector("body").innerHTML = data + curr ;
+    });
+
+};
+
+includeNavBar();
