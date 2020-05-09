006da4f1bf311874e5f6d817ffc538f8f659483b
Matteo Di Stadio <distadio.1794111@studenti.uniroma1.it>
2020-05-10 00:08:12 +0200

    continuato lavoro su client battaglia e aggiunti esempi di background attacchi

---
+++ Site/lib/js/battaglia/testing.js
@@ -0,0 +1,203 @@
+var A = {
+    "primo": {
+			"utente": "Red",
+			"azione": "mossa_attesa",
+			"valore": 13,
+			"danno": 1000,
+            "comunicato": "Brutto Colpo!"
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "_switch",
+            "valore": 0,
+            "danno": 0,
+            "comunicato": ""
+    }
+};
+
+var B = {
+    "primo": {
+			"utente": "Red",
+			"azione": "mossa_vinto",
+			"valore": 13,
+			"danno": 1000,
+            "comunicato": "Superefficace!"
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "_perso",
+            "valore": 0,
+            "danno": 0,
+            "comunicato": ""
+    }
+};
+
+var C = {
+    "primo": {
+			"utente": "Red",
+			"azione": "mossa_switch",
+			"valore": 13,
+			"danno": 1,
+            "comunicato": "Superefficace!"
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "mossa_attesa",
+            "valore": 13,
+            "danno": 1000,
+            "comunicato": "Brutto colpo!"
+    }
+};
+
+var D = {
+    "primo": {
+			"utente": "Red",
+			"azione": "mossa_perso",
+			"valore": 13,
+			"danno": 1,
+            "comunicato": "Superefficace!"
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "mossa_vinto",
+            "valore": 13,
+            "danno": 1000,
+            "comunicato": "Brutto colpo!"
+    }
+};
+
+var E = {
+    "primo": {
+			"utente": "Red",
+			"azione": "mossa_",
+			"valore": 13,
+			"danno": 1,
+            "comunicato": "Superefficace!"
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "mossa_",
+            "valore": 13,
+            "danno": 1,
+            "comunicato": "Brutto colpo!"
+    }
+};
+
+var F = {
+    "primo": {
+			"utente": "Red",
+			"azione": "switch_switch",
+			"valore": 1,
+			"danno": 0,
+            "comunicato": ""
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "mossa_attesa",
+            "valore": 13,
+            "danno": 1000,
+            "comunicato": ""
+    }
+};
+
+var G = {
+    "primo": {
+			"utente": "Red",
+			"azione": "switch_",
+			"valore": 1,
+			"danno": 0,
+            "comunicato": ""
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "mossa_",
+            "valore": 13,
+            "danno": 1,
+            "comunicato": ""
+    }
+};
+
+var H = {
+    "primo": {
+			"utente": "Red",
+			"azione": "switch_",
+			"valore": 1,
+			"danno": 0,
+            "comunicato": ""
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "switch_",
+            "valore": 1,
+            "danno": 0,
+            "comunicato": ""
+    }
+};
+
+var I = {
+    "primo": {
+			"utente": "Red",
+			"azione": "switch_",
+			"valore": 1,
+			"danno": 0,
+            "comunicato": ""
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "attesa_",
+            "valore": 0,
+            "danno": 0,
+            "comunicato": ""
+    }
+};
+
+var J = {
+    "primo": {
+			"utente": "Red",
+			"azione": "forfeit_perso",
+			"valore": 0,
+			"danno": 0,
+            "comunicato": ""
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "forfeit_perso",
+            "valore": 0,
+            "danno": 0,
+            "comunicato": ""
+    }
+};
+
+var K = {
+    "primo": {
+			"utente": "Red",
+			"azione": "forfeit_perso",
+			"valore": 0,
+			"danno": 0,
+            "comunicato": ""
+    },
+    "secondo": {
+            "utente": "Blue",
+            "azione": "_vinto",
+            "valore": 0,
+            "danno": 0,
+            "comunicato": ""
+    }
+};
+
+var L = {
+    "primo": {
+			"utente": "Blue",
+			"azione": "forfeit_perso",
+			"valore": 0,
+			"danno": 0,
+            "comunicato": ""
+    },
+    "secondo": {
+            "utente": "Red",
+            "azione": "_vinto",
+            "valore": 0,
+            "danno": 0,
+            "comunicato": ""
+    }
+};
