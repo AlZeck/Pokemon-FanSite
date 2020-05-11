a296b4a487f2d8ce65e84e3b2b6d2f0694fa98e3
Matteo Di Stadio <distadio.1794111@studenti.uniroma1.it>
2020-05-11 13:15:56 +0200

    continuato lavoro su client battaglia e sistemato problema asincronismo, prendita dal DB e animazioni

--- Site/lib/js/battaglia/testing.js
+++ Site/lib/js/battaglia/testing.js
@@ -1,3 +1,4 @@
+//esempi messaggi json che arrivano al client
 //da provare scambiando gli utenti anche
 
 var A = {
@@ -98,7 +99,7 @@ var F = {
             "azione": "mossa_attesa",
             "valore": 13,
             "danno": 1000,
-            "comunicato": ""
+            "comunicato": "Superefficace!"
     }
 };
 
@@ -115,7 +116,7 @@ var G = {
             "azione": "mossa_",
             "valore": 13,
             "danno": 1,
-            "comunicato": ""
+            "comunicato": "Superefficace!"
     }
 };
 
@@ -186,3 +187,53 @@ var K = {
             "comunicato": ""
     }
 };
+
+
+//---------------------------------------------------------------------------------------------------
+
+
+//funzione per prendere dal DB un pokemon o una mossa (specificato con stringa omonima in parametro "cosa") tramite loro id
+
+function prendiDalDB(cosa, id) {
+    var ret;
+
+    $.ajax({
+        url: "/lib/php/battleInfo.php?type=" + cosa + "&id=" + id,
+        async: false,
+        dataType: 'json',
+        success: function (response) {
+            if(cosa == "pokemon") {
+                ret = {
+                    id: parseInt(response.id),
+                    nome: response.nome.charAt(0).toUpperCase() + response.nome.slice(1),
+                    tipo1: response.tipo1,
+                    tipo2: response.tipo2,
+                    psMax: parseInt(response.ps),
+                    ps: parseInt(response.ps),
+                    att: parseInt(response.att),
+                    dif: parseInt(response.dif),
+                    atts: parseInt(response.attsp),
+                    difs: parseInt(response.difsp),
+                    vel: parseInt(response.vel), 
+                    mini_sprite: "/assets/pokemon/mini_sprite/" + response.nome + ".png",
+                    front_sprite: "/assets/pokemon/front_sprite/" + response.nome + ".gif",
+                    back_sprite: "/assets/pokemon/back_sprite/" + response.nome + ".gif",
+                    mosse: []
+                };
+                
+            }
+            else {
+                ret = {
+                    id: parseInt(response.id),
+                    nome: response.nome,
+                    tipo: response.tipo,
+                    potenza: parseInt(response.potenza),
+                    precisione: parseInt(response.precisione),
+                    categoria: response.categoria
+                };
+            }
+        }
+    });
+
+    return ret;
+}
