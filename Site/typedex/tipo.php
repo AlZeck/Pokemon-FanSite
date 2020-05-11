2680603fc9900156679845f3926ac564ae113196
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-05-11 18:51:45 +0200

    completed specific type page

--- Site/typedex/tipo.php
+++ Site/typedex/tipo.php
@@ -1,8 +1,139 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pokedex-PokemonFan Site</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="/lib/css/tipo.css" rel="stylesheet">
+    <link href="/lib/css/stat.css" rel="stylesheet">
+    <link href="/lib/css/tipi.css" rel="stylesheet">
+    <script src="/lib/js/navbar.js"></script>
+
+</head>
+
 <?php
-    include '../lib/php/dbcontroller.php';
-    $con = DBController::getController();
-    $mvs = $con->getListMovesByType($_GET["id"]);
-    var_dump($mvs);
-    $pks = $con->getListPokemonByType($_GET["id"]);
-    var_dump($pks);
+include '../lib/php/dbcontroller.php';
+$con = DBController::getController();
+$tipo = $_GET["id"];
+$mvs = $con->getListMovesByType($tipo);
+$pks = $con->getListPokemonByType($tipo);
+$eff = ["superefficace" => "Superefficace", "poco_efficace" => "Poco efficace", "inefficace" => "Inefficace"];
+
+if ($con->checkTipoExists($tipo)) {
+    $raw_data  = file_get_contents('../assets/efficacie_tipo/' . $tipo . '.json');
+    $data = json_decode($raw_data, true);
+} else {
+    //header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+    include("../lib/php/notFound.php");
+    sendError("Tipo not found");
+    die();
+}
 ?>
+
+<body>
+    <br>
+    <div class="container">
+        <h2 class="type-header"><?php echo ucfirst($tipo); ?></h2>
+
+        <div class="row">
+            <div class="col">
+                <div class="card eff-card">
+                    <div class="card-body">
+                        <h4 class="card-title">Offensivo</h3>
+                            <?php
+                            $lis = $data['offensivo'];
+                            foreach ($eff as $name => $title) {
+                                echo '<strong>' . $title . '</strong>';
+                                echo '<div class="card-deck">';
+                                if ($lis[$name] != []) {
+                                    foreach ($lis[$name] as $currtype) {
+                                        echo '<a class="btn btn-tipo ' . $currtype . '"' .
+                                            'href="/typedex/tipo.php?id=' . $currtype . '">'
+                                            . strtoupper($currtype) . '</a>';
+                                    }
+                                }
+                                echo '</div>';
+                            }
+                            ?>
+                    </div>
+                </div>
+                <br>
+            </div>
+            <div class="col">
+                <div class="card eff-card">
+                    <div class="card-body">
+                        <h4 class="card-title">Diffensivo</h3>
+                            <?php
+                            $lis = $data['diffensivo'];
+                            foreach ($eff as $name => $title) {
+                                echo '<strong>' . $title . '</strong>';
+                                echo '<div class="card-deck">';
+                                if ($lis[$name] != []) {
+                                    foreach ($lis[$name] as $currtype) {
+                                        echo '<a class="btn btn-tipo ' . $currtype . '"' .
+                                            'href="/typedex/tipo.php?id=' . $currtype . '">'
+                                            . strtoupper($currtype) . '</a>';
+                                    }
+                                }
+                                echo '</div>';
+                            }
+                            ?>
+                    </div>
+                </div>
+                <br>
+            </div>
+
+
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title"> Pokemon </h3>
+                <div class="card-deck">
+                    <?php
+                    foreach ($pks as $pokemon) {
+
+                        echo '<div class="mb-4">' .
+                            '    <div class="card pokecard">' .
+                            '        <a href="/pokedex/pokemon.php?id=' . $pokemon["id"] . '">' .
+                            '        <div class="card-body">' .
+                            '           <h5 class="card-title"> <span class="number"> #' . $pokemon["id"] . '</span> ' .
+                            '                ' . ucfirst($pokemon["nome"]) . '</h5>' .
+                            '           <img src="/assets/pokemon/artwork/' . $pokemon["nome"] . '.png" alt="' . $pokemon["nome"] . '">' .
+                            '        </div>' .
+                            '        </a>' .
+                            '    </div>' .
+                            '</div>';
+                    }
+                    ?>
+
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title"> Mosse </h3>
+                <div class="card-deck">
+                    <?php
+                    foreach ($mvs as $mossa) {
+                        echo '<div class="mb-4">' .
+                            '   <a class="card btn ' . $mossa["tipo"] . '" style="width: 185px; align-items:center; font-weight: bold;" ' .
+                            '       href="/movedex/mossa.php?id=' . $mossa["id"] . '">' . ucfirst($mossa["nome"]) .
+                            '   </a>' .
+                            '</div>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+</body>
+
+</html>
