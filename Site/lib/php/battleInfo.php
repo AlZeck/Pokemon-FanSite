623d03b2482f037d4ea403da8383670013b0b32b
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-05-11 08:57:16 +0200

    added search by name function and json header

--- Site/lib/php/battleInfo.php
+++ Site/lib/php/battleInfo.php
@@ -6,6 +6,8 @@ if($_GET["type"]=="pokemon"){
     $info = $con->getPokemonById($_GET["id"]);
 }elseif($_GET["type"]=="mossa"){
     $info = $con->getMossaById($_GET["id"]);
+}elseif($_GET["type"]=="search"){
+    $info = $con->searchByName($_GET["name"]);
 }
 echo json_encode($info);
 ?>
