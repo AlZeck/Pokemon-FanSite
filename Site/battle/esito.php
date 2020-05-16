c7d068a4949a6694eacf83babab755b27371c67a
andrea-cerone <andreacerone97@yahoo.it>
2020-05-16 18:28:35 +0200

    modifiche  esito.php

--- Site/battle/esito.php
+++ Site/battle/esito.php
@@ -24,10 +24,12 @@
         <?php 
             if($_GET['risultato']=='vittoria')
                 echo '<img src="/assets/img/risultato/winning.png" class="d-block w-100" alt="vittoria">
+                    <br>               
                     <h3>CONGRATULAZIONI HAI VINTO LA BATTAGLIA!</h3>';
 
             else
                 echo '<img src="/assets/img/risultato/losing.png" class="d-block w-100" alt="sconfitta">
+                    <br>
                     <h3>PURTROPPO HAI PERSO LA BATTAGLIA!</h3>'
         ?>
         <br>
