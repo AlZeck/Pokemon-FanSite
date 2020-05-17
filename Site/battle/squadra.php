d0c15a6a9d95fea587793541a1981f4e1a2ad7cd
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-05-17 13:37:47 +0200

    ui improvements + optimizations

--- Site/battle/squadra.php
+++ Site/battle/squadra.php
@@ -1,14 +1,14 @@
 <?php
-	if (!isset($_COOKIE['PHPSESSID']) || !isset($_COOKIE['user'])) {
-		include "index.html";
-		die();
-	}
-	session_start();
-	$user = $_COOKIE['user'];
+if (!isset($_COOKIE['PHPSESSID']) || !isset($_COOKIE['user'])) {
+	include "index.html";
+	die();
+}
+session_start();
+$user = $_COOKIE['user'];
 
-	include '../lib/php/dbcontroller.php';
-	$con = DBController::getController();
-	$lis = $con->getPokemonList();
+include '../lib/php/dbcontroller.php';
+$con = DBController::getController();
+$lis = $con->getPokemonList();
 ?>
 
 <!DOCTYPE html>
@@ -29,8 +29,8 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
 	<link href="/lib/css/stat.css" rel="stylesheet">
-	<link href="/lib/css/tipi.css" rel="stylesheet">
 	<link href="/lib/css/squadra.css" rel="stylesheet">
+	<link href="/lib/css/tipi.css" rel="stylesheet">
 	<script>
 		function myFunction() {
 			// Declare variables
@@ -52,179 +52,162 @@
 			}
 		}
 	</script>
+	<style>
+	</style>
 
 	<script type="text/javascript" lang="javascript" src="/lib/js/battaglia/vue.min.js"></script>
 	<script type="text/javascript" lang="javascript" src="/lib/js/battaglia/prendi_dal_db.js"></script>
 	<script type="text/javascript" lang="javascript" src="/lib/js/battaglia/squadra_casuale.js"></script>
-    <script type="text/javascript" lang="javascript" src="/lib/js/scelta_squadra/sceltavue.js"></script>
+
 </head>
 
 <body>
-<span id="sceltaVue">
+	<div id="sceltaVue">
 
-	<nav class="navbar sticky-top navbar-expand-sm navbar-dark pokenavbar">
-		<div class="navbar-nav mr-auto border-right">
-			<button type="button" class="btn btn-danger mr-4 ml-2" onclick="return window.location.assign('/')">ESCI</button>
-		</div>
-		<div class="navbar-nav mx-auto">
-			<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(1)"> <img v-bind:src="primoPkm.mini_sprite" alt="mini-sprite"> {{ primoPkm.nome }} </div>
-			<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(2)"> <img v-bind:src="secondoPkm.mini_sprite" alt="mini-sprite"> {{ secondoPkm.nome }} </div>
-			<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(3)"> <img v-bind:src="terzoPkm.mini_sprite" alt="mini-sprite"> {{ terzoPkm.nome }} </div>
-			<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(4)"> <img v-bind:src="quartoPkm.mini_sprite" alt="mini-sprite"> {{ quartoPkm.nome }} </div>
-			<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(5)"> <img v-bind:src="quintoPkm.mini_sprite" alt="mini-sprite"> {{ quintoPkm.nome }} </div>
-			<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(6)"> <img v-bind:src="sestoPkm.mini_sprite" alt="mini-sprite"> {{ sestoPkm.nome }} </div>
-		</div>
-		<div class="navbar-nav ml-auto border-left">
-			<button type="button" class="btn btn-primary ml-4 mr-2" v-on:click="casuale">CASUALE</button>
-			<button type="button" class="btn btn-success ml-2 mr-2" v-on:click="conferma">CONFERMA</button>
-		</div>
-	</nav>
-	<div class="container-fluid">
-		<div class="row" style="height:100%">
+		<nav class="navbar sticky-top navbar-expand-sm navbar-dark pokenavbar">
+			<div class="navbar-nav mr-auto border-right">
+				<button type="button" class="btn btn-danger mr-4 ml-2" onclick="return window.location.assign('/')">ESCI</button>
+			</div>
+			<div class="navbar-nav mx-auto">
+				<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(1)"> <img v-bind:src="primoPkm.mini_sprite" alt="mini-sprite"> {{ primoPkm.nome }} </div>
+				<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(2)"> <img v-bind:src="secondoPkm.mini_sprite" alt="mini-sprite"> {{ secondoPkm.nome }} </div>
+				<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(3)"> <img v-bind:src="terzoPkm.mini_sprite" alt="mini-sprite"> {{ terzoPkm.nome }} </div>
+				<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(4)"> <img v-bind:src="quartoPkm.mini_sprite" alt="mini-sprite"> {{ quartoPkm.nome }} </div>
+				<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(5)"> <img v-bind:src="quintoPkm.mini_sprite" alt="mini-sprite"> {{ quintoPkm.nome }} </div>
+				<div class="nav-link active btn pokebtn" v-on:click="cambiaSelectedPkmSquadra(6)"> <img v-bind:src="sestoPkm.mini_sprite" alt="mini-sprite"> {{ sestoPkm.nome }} </div>
+			</div>
+			<div class="navbar-nav ml-auto border-left">
+				<button type="button" class="btn btn-primary ml-4 mr-2" v-on:click="casuale">CASUALE</button>
+				<button type="button" class="btn btn-success ml-2 mr-2" v-on:click="conferma" :disabled="!squadraPronta">CONFERMA</button>
+			</div>
+		</nav>
+		<div class="container-fluid">
+			<div class="row" style="height:100%">
 
-			<div class="col-lg-8">
-				<br>
-				<input type="text" class="form-control" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
-				<br>
-				<div id="pokelist" class="card-deck scroll pb-4">
+				<div class="col-lg-8">
+					<div class="onbar">
+						<input type="text" class="form-control" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+					</div>
+					<div id="pokelist" class="card-deck scroll pb-4">
 
-					<?php
+						<?php
 						foreach ($lis as $pokemon) {
 
 							echo    '<div class="mt-4">' .
-								'<div class="card" value="' . $pokemon["id"] . '" v-on:click="vediPkm">' .
+								'<div class="card" v-on:click="selezionaPkm(' . $pokemon["id"] . ')">' .
 								'<div class="card-body">' .
 								'<h5 class="card-title"> <span class="number">#' . $pokemon["id"] . ' </span>'
 								. ucfirst($pokemon["nome"]) . '</h5>' .
-								'<img src="/assets/pokemon/artwork/' . $pokemon["nome"] . '.png" alt="' . $pokemon["nome"] . '">' .
+								'<img src="/assets/pokemon/artwork_small/' . $pokemon["nome"] . '.png" alt="' . $pokemon["nome"] . '">' .
 								'</div>' .
 								'</div>' .
 								'</div>';
 						}
-					?>
+						?>
 
-				</div>
-			</div>
-
-			<div id="selectedPkm" class="col-lg-4 scroll pokeinfo">
-				<div class="btn-toolbar justify-content-between" role="toolbar">
-					<div role="group">
-						<img v-bind:src="selectedPkm.artwork" alt="artwork">
 					</div>
+				</div>
 
-					<div role="group">
-						<h3> {{ selectedPkm.nome }} </h3>
+				<div class="col-lg-4 pokeinfo">
+					<div id="selectedPkm">
+						<div class="btn-toolbar onbar justify-content-between" role="toolbar">
+							<div role="group">
+								<h3> {{ selectedPkm.nome }} </h3>
+							</div>
 
-						<br>
+							<div class="btn-group">
+								<span class="btn btn-tipo" v-bind:class="selectedPkm.tipo1"> {{ selectedPkm.tipo1 }} </span>
+								<span v-if="selectedPkm.tipo2 != null" class="btn btn-tipo" v-bind:class="selectedPkm.tipo2"> {{ selectedPkm.tipo2 }} </span>
+							</div>
 
-						<div class="btn-group">
-							<span class="btn btn-tipo" v-bind:class="selectedPkm.tipo1"> {{ selectedPkm.tipo1 }} </span>
-							<span v-if="selectedPkm.tipo2 != null" class="btn btn-tipo" v-bind:class="selectedPkm.tipo2"> {{ selectedPkm.tipo2 }} </span>
 						</div>
+						<div class="scroll">
+							<br>
 
-						<br>
+							<img v-bind:src="selectedPkm.artwork" alt="artwork">
+							{{ selectedPkm.uber }}
 
-						{{ selectedPkm.uber }}
+							<br>
 
-						<br>
+							<div class="card">
+								<div class="card-body card-stat">
+									<div class="row">
+										<div class="col-sm-2 stat-label">PS</div>
+										<div class="col">
+											<div class="progress justify-content-between">
+												<div class="progress-bar bar-stat-ps" role="progressbar" v-bind:aria-valuenow="selectedPkm.ps" aria-valuemin="0" v-bind:aria-valuemax="270" :style="{ width: selectedPkm.ps/270*100 + '%'} ">
+													{{selectedPkm.ps}} </div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-2 stat-label">ATT</div>
+										<div class="col">
+											<div class="progress justify-content-between">
+												<div class="progress-bar bar-stat-att" role="progressbar" v-bind:aria-valuenow="selectedPkm.att" aria-valuemin="0" v-bind:aria-valuemax="270" :style="{ width: selectedPkm.att/270*100 + '%' }">
+													{{selectedPkm.att}} </div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-2 stat-label">DIF</div>
+										<div class="col">
+											<div class="progress justify-content-between">
+												<div class="progress-bar bar-stat-dif" role="progressbar" v-bind:aria-valuenow="selectedPkm.dif" aria-valuemin="0" v-bind:aria-valuemax="270" :style="{ width: selectedPkm.dif/270*100 + '%' }">
+													{{selectedPkm.dif}} </div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-2 stat-label">ATTS</div>
+										<div class="col">
+											<div class="progress justify-content-between">
+												<div class="progress-bar bar-stat-attsp" role="progressbar" v-bind:aria-valuenow="selectedPkm.atts" aria-valuemin="0" v-bind:aria-valuemax="270" :style="{ width: selectedPkm.atts/270*100 + '%' }">
+													{{selectedPkm.atts}} </div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-2 stat-label">DIFS</div>
+										<div class="col">
+											<div class="progress justify-content-between">
+												<div class="progress-bar bar-stat-difsp" role="progressbar" v-bind:aria-valuenow="selectedPkm.difs" aria-valuemin="0" v-bind:aria-valuemax="270" :style="{ width: selectedPkm.difs/270*100 + '%' }">
+													{{selectedPkm.difs}} </div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-2 stat-label">VEL</div>
+										<div class="col">
+											<div class="progress justify-content-between">
+												<div class="progress-bar bar-stat-vel" role="progressbar" v-bind:aria-valuenow="selectedPkm.vel" aria-valuemin="0" v-bind:aria-valuemax="270" :style="{ width: selectedPkm.vel/270*100 + '%' }">
+													{{selectedPkm.vel}} </div>
+											</div>
+										</div>
 
-						<div class="card">
-							<div class="card-body card-stat">
-								<div class="row">
-									<div class="col-sm-2 stat-label">PS</div>
-									<div class="col">
-										<div class="progress justify-content-between">
-											<div class="progress-bar bar-stat-ps" role="progressbar"
-												v-bind:aria-valuenow="selectedPkm.ps" aria-valuemin="0"
-												v-bind:aria-valuemax="270"
-												:style="{ width: selectedPkm.ps/270*100 + '%'} ">
-												{{selectedPkm.ps}} </div>
-										</div>
 									</div>
-								</div>
-								<div class="row">
-									<div class="col-sm-2 stat-label">ATT</div>
-									<div class="col">
-										<div class="progress justify-content-between">
-											<div class="progress-bar bar-stat-att" role="progressbar"
-												v-bind:aria-valuenow="selectedPkm.att" aria-valuemin="0"
-												v-bind:aria-valuemax="270"
-												:style="{ width: selectedPkm.att/270*100 + '%' }">
-												{{selectedPkm.att}} </div>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-sm-2 stat-label">DIF</div>
-									<div class="col">
-										<div class="progress justify-content-between">
-											<div class="progress-bar bar-stat-dif" role="progressbar"
-												v-bind:aria-valuenow="selectedPkm.dif" aria-valuemin="0"
-												v-bind:aria-valuemax="270"
-												:style="{ width: selectedPkm.dif/270*100 + '%' }">
-												{{selectedPkm.dif}} </div>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-sm-2 stat-label">ATTS</div>
-									<div class="col">
-										<div class="progress justify-content-between">
-											<div class="progress-bar bar-stat-attsp" role="progressbar"
-												v-bind:aria-valuenow="selectedPkm.atts" aria-valuemin="0"
-												v-bind:aria-valuemax="270"
-												:style="{ width: selectedPkm.atts/270*100 + '%' }">
-												{{selectedPkm.atts}} </div>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-sm-2 stat-label">DIFS</div>
-									<div class="col">
-										<div class="progress justify-content-between">
-											<div class="progress-bar bar-stat-difsp" role="progressbar"
-												v-bind:aria-valuenow="selectedPkm.difs" aria-valuemin="0"
-												v-bind:aria-valuemax="270"
-												:style="{ width: selectedPkm.difs/270*100 + '%' }">
-												{{selectedPkm.difs}} </div>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-sm-2 stat-label">VEL</div>
-									<div class="col">
-										<div class="progress justify-content-between">
-											<div class="progress-bar bar-stat-vel" role="progressbar"
-												v-bind:aria-valuenow="selectedPkm.vel" aria-valuemin="0"
-												v-bind:aria-valuemax="270"
-												:style="{ width: selectedPkm.vel/270*100 + '%' }">
-												{{selectedPkm.vel}} </div>
-										</div>
-									</div>
-
 								</div>
 							</div>
+
+							<br>
+
+							<!-- FARE CHE SE UNA MOSSA ANCORA NON PRESA C'È UN RIQUADRO DI DEFAULT -->
+							<button class="card card-body" v-for="m in selectedPkm.mosse" v-bind:value="m.id" v-bind:class="m.tipo">
+								<h5 class="card-title">{{ m.nome }} </h5>
+								{{ m.tipo.toUpperCase() }} {{ m.categoria.toUpperCase() }} <br>
+								{{ m.potenza }} - {{ m.precisione }}
+							</button>
+
+							<br>
+
+							<!-- QUI METTERE LISTA MOSSE DISPONIBILI -->
 						</div>
 					</div>
 				</div>
-
-				<br>
-
-				<!-- FARE CHE SE UNA MOSSA ANCORA NON PRESA C'È UN RIQUADRO DI DEFAULT -->
-				<button class="card card-body" v-for="m in selectedPkm.mosse" v-bind:value="m.id" v-bind:class="m.tipo">
-					<h5 class="card-title">{{ m.nome }} </h5>
-					{{ m.tipo.toUpperCase() }} {{ m.categoria.toUpperCase() }} <br>
-					{{ m.potenza }} - {{ m.precisione }}
-                </button>
-
-				<br>
-
-				<!-- QUI METTERE LISTA MOSSE DISPONIBILI -->
 			</div>
 		</div>
-	</div>
 
-	<!-- <div class="container">
+		<!-- <div class="container">
 		<h1 class="display-4">Ciao, Pokefan!</h1>
 		<p class="lead"> Purtroppo il nostro simulatore di battaglia non è un ancora disponibile,
 			siamo un gruppo di studenti amanti a i pokemon e lavoriamo quanto possibile per avere tutto funzionante il prima possibile.</p>
@@ -235,5 +218,10 @@
 		</p>
 	</div> -->
 
-</span>
+	</div>
+
+
 </body>
+<script type="text/javascript" lang="javascript" src="/lib/js/scelta_squadra/sceltavue.js"></script>
+
+</html>
--- Site/lib/js/scelta_squadra/sceltavue.js
+++ Site/lib/js/scelta_squadra/sceltavue.js
@@ -1,168 +1,3 @@
-//per caricare l'oggetto vue con le sue informazioni al caricamento della pagina
-window.addEventListener("load", function() {
-
-    //oggetto vue per le informazioni del gioco
-    var sceltaVueObj = {
-        //id dell'element associato
-        el: '#sceltaVue',
-
-        data: {
-            //l'oggetto pokemon selezionato (inizialmente defaultPkm)
-            selectedPkm: defaultPkm,
-
-            //la squadra corrente, in posizione 0 ci sta il pokemon di default
-            squadra: [ defaultPkm ],
-
-            //il json della squadra completa da inviare poi al server (casuale lo inizializza subito)
-            squadraJSON: ""
-        },
-
-        computed: {
-            //il computed per ottenere il pokemon selezionato del protagonista in base al valore del suo indice
-            selectedPkm: function() {
-                return this.selectedPkm;
-            },
-
-
-            //il computed per ottenere il primo pokemon della squadra
-            primoPkm: function() {
-                if(this.squadra.length >= 2) return this.squadra[1];
-                else return this.squadra[0];
-            },
-
-
-            //il computed per ottenere il primo pokemon della squadra
-            secondoPkm: function() {
-                if(this.squadra.length >= 3) return this.squadra[2];
-                else return this.squadra[0];
-            },
-
-
-            //il computed per ottenere il primo pokemon della squadra
-            terzoPkm: function() {
-                if(this.squadra.length >= 4) return this.squadra[3];
-                else return this.squadra[0];
-            },
-
-
-            //il computed per ottenere il primo pokemon della squadra
-            quartoPkm: function() {
-                if(this.squadra.length >= 5) return this.squadra[4];
-                else return this.squadra[0];
-            },
-
-
-            //il computed per ottenere il primo pokemon della squadra
-            quintoPkm: function() {
-                if(this.squadra.length >= 6) return this.squadra[5];
-                else return this.squadra[0];
-            },
-
-
-            //il computed per ottenere il primo pokemon della squadra
-            sestoPkm: function() {
-                if(this.squadra.length >= 7) return this.squadra[6];
-                else return this.squadra[0];
-            }
-        },
-
-        methods: {
-            //metodo per cambiare il pokemon selezionato dell'allenatore in base all'indice passato e se il pokemon è noto o meno
-            cambiaSelectedPkmSquadra: function(i) {
-                if(i < this.squadra.length) this.selectedPkm = this.squadra[i];
-            },
-
-
-            //metodo chiamato premendo il pulsante di creazione squadra casuale
-            casuale: function() {
-                this.squadraJSON = creaSquadra();
-
-                var nuovaSquadra = [ defaultPkm ];
-
-                var i, j;
-                for(i=0; i<6; i++) {
-                    nuovaSquadra.push( prendiDalDB("pokemon", this.squadraJSON[i].id) );
-
-                    for(j=0; j<4; j++) {
-                        nuovaSquadra[i+1].mosse.push( prendiDalDB("mossa", this.squadraJSON[i].mosse[j]) );
-                    }
-                }
-
-                this.squadra = nuovaSquadra;
-                this.cambiaSelectedPkmSquadra(1);
-            },
-
-
-            //metodo che controlla se la squadra è stata completamente inizializzata
-            squadraPronta() {
-                if(this.squadra.length < 7) return false;
-
-                var i;
-                for(i=1; i<7; i++) {
-                    if(this.squadra[i].mosse.length < 4) return false;
-                }
-
-                return true;
-            },
-
-
-            //DA MODIFICARE: deve creare il battVue, bcpcontroller e inizializzare la battaglia e reindirizzare nella pagina apposita se ha successo
-            //metodo chiamato premendo il pulsante di conferma
-            conferma: function() {
-                //non hai la squadra ancora pronta
-                if(! this.squadraPronta()) {
-                    //testing
-                    console.log("NO");
-                }
-
-                else {
-                    if(this.squadraJSON == "") {
-                        var sjson = [];
-
-                        var i, j;
-                        for(i=1; i<7; i++) {
-                            sjson.push( {"id": this.squadra[i].id, "mosse": []} );
-
-                            for(j=0; j<4; j++) {
-                                sjson[i-1].mosse.push( this.squadra[i].mosse[j].id );
-                            }
-                        }
-
-                        this.squadraJSON = sjson;
-                    }
-    
-                    //testing
-                    console.log(this.squadraJSON);
-                }
-            },
-
-
-            //metodo che mette il pokemon selezionato come quello selezionato
-            vediPkm: function(e) {
-                console.log(e.currentTarget.value);
-            }
-
-
-            /*
-                fare un metodo che cliccando su una delle card e sinistra diventa pokemon selezionato
-
-                fare un metodo che se si sta su un selezionato che sta già in squadra si può togliere dalla squadra
-
-                fare un metodo che cliccando su una delle mosse disponibili nella card del selezionato si può aggiungere alle mosse scelte
-
-                fare un metodo che si può togliere una delle mosse scelte del pokemon selezionato
-            */
-        }
-    };
-
-
-
-    //istanzio nuovo oggetto Vue
-    sceltaVue = new Vue(sceltaVueObj);
-});
-
-
-
 //variabile per poterlo usare al di fuori
 var sceltaVue;
 
@@ -179,7 +14,7 @@ var defaultPkm = {
     dif: 0,
     atts: 0,
     difs: 0,
-    vel: 0, 
+    vel: 0,
     mini_sprite: "/assets/pokemon/default_sprites/default_mini.png",
     artwork: "/assets/pokemon/default_sprites/default_front_back.png",
     mosse: []
@@ -198,8 +33,177 @@ var bulbasaur = {
     dif: 0,
     atts: 0,
     difs: 0,
-    vel: 0, 
+    vel: 0,
     mini_sprite: "/assets/pokemon/mini_sprite/bulbasaur.png",
     artwork: "/assets/pokemon/artwork/bulbasaur.png",
     mosse: []
 }
+
+
+//per caricare l'oggetto vue con le sue informazioni al caricamento della pagina
+//window.addEventListener("load", function() {
+
+//oggetto vue per le informazioni del gioco
+var sceltaVueObj = {
+    //id dell'element associato
+    el: '#sceltaVue',
+
+    data: {
+        //l'oggetto pokemon selezionato (inizialmente defaultPkm)
+        selectedPkm: defaultPkm,
+
+        //la squadra corrente, in posizione 0 ci sta il pokemon di default
+        squadra: [defaultPkm],
+
+        //il json della squadra completa da inviare poi al server (casuale lo inizializza subito)
+        squadraJSON: ""
+    },
+
+    computed: {
+        //il computed per ottenere il pokemon selezionato del protagonista in base al valore del suo indice
+        selectedPkm: function () {
+            return this.selectedPkm;
+        },
+
+
+        //il computed per ottenere il primo pokemon della squadra
+        primoPkm: function () {
+            if (this.squadra.length >= 2) return this.squadra[1];
+            else return this.squadra[0];
+        },
+
+
+        //il computed per ottenere il primo pokemon della squadra
+        secondoPkm: function () {
+            if (this.squadra.length >= 3) return this.squadra[2];
+            else return this.squadra[0];
+        },
+
+
+        //il computed per ottenere il primo pokemon della squadra
+        terzoPkm: function () {
+            if (this.squadra.length >= 4) return this.squadra[3];
+            else return this.squadra[0];
+        },
+
+
+        //il computed per ottenere il primo pokemon della squadra
+        quartoPkm: function () {
+            if (this.squadra.length >= 5) return this.squadra[4];
+            else return this.squadra[0];
+        },
+
+
+        //il computed per ottenere il primo pokemon della squadra
+        quintoPkm: function () {
+            if (this.squadra.length >= 6) return this.squadra[5];
+            else return this.squadra[0];
+        },
+
+
+        //il computed per ottenere il primo pokemon della squadra
+        sestoPkm: function () {
+            if (this.squadra.length >= 7) return this.squadra[6];
+            else return this.squadra[0];
+        },
+
+
+        //metodo che controlla se la squadra è stata completamente inizializzata
+        squadraPronta() {
+            if (this.squadra.length < 7) return false;
+
+            var i;
+            for (i = 1; i < 7; i++) {
+                if (this.squadra[i].mosse.length < 4) return false;
+            }
+
+            return true;
+        },
+    },
+
+    methods: {
+        //metodo per cambiare il pokemon selezionato dell'allenatore in base all'indice passato e se il pokemon è noto o meno
+        cambiaSelectedPkmSquadra: function (i) {
+            if (i < this.squadra.length) this.selectedPkm = this.squadra[i];
+        },
+
+
+        //metodo chiamato premendo il pulsante di creazione squadra casuale
+        casuale: function () {
+            this.squadraJSON = creaSquadra();
+
+            var nuovaSquadra = [defaultPkm];
+
+            var i, j;
+            for (i = 0; i < 6; i++) {
+                nuovaSquadra.push(prendiDalDB("pokemon", this.squadraJSON[i].id));
+
+                for (j = 0; j < 4; j++) {
+                    nuovaSquadra[i + 1].mosse.push(prendiDalDB("mossa", this.squadraJSON[i].mosse[j]));
+                }
+            }
+
+            this.squadra = nuovaSquadra;
+            this.cambiaSelectedPkmSquadra(1);
+        },
+
+
+        //DA MODIFICARE: deve creare il battVue, bcpcontroller e inizializzare la battaglia e reindirizzare nella pagina apposita se ha successo
+        //metodo chiamato premendo il pulsante di conferma
+        conferma: function () {
+            //non hai la squadra ancora pronta
+            if (!this.squadraPronta) {
+                //testing
+                console.log("NO");
+            }
+
+            else {
+                if (this.squadraJSON == "") {
+                    var sjson = [];
+
+                    var i, j;
+                    for (i = 1; i < 7; i++) {
+                        sjson.push({ "id": this.squadra[i].id, "mosse": [] });
+
+                        for (j = 0; j < 4; j++) {
+                            sjson[i - 1].mosse.push(this.squadra[i].mosse[j].id);
+                        }
+                    }
+
+                    this.squadraJSON = sjson;
+                }
+
+                //testing
+                console.log(this.squadraJSON);
+            }
+        },
+
+
+        //metodo che mette il pokemon selezionato come quello selezionato (appena si ha bottone renderlo disabled se squadra.includes(selectedPkm))
+        selezionaPkm: function (id) {
+            var i;
+            for (i = 1; i < this.squadra.length; i++) {
+                if (this.squadra[i].id == id) {
+                    this.selectedPkm = this.squadra[i];
+                    return;
+                }
+            }
+
+            this.selectedPkm = prendiDalDB("pokemon", id);
+        }
+
+    /*
+        fare un metodo che se si sta su un selezionato che sta già in squadra si può togliere dalla squadra
+
+        fare un metodo che cliccando su una delle mosse disponibili nella card del selezionato si può aggiungere alle mosse scelte
+
+        fare un metodo che si può togliere una delle mosse scelte del pokemon selezionato
+    */
+}
+};
+
+
+
+//istanzio nuovo oggetto Vue
+sceltaVue = new Vue(sceltaVueObj);
+//});
