e8de5ff2962ca51e08e04c3edda0dca1f2416cbe
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-05-19 12:50:07 +0200

    fix double navbar
    Co-authored-by: Matteo Di Stadio <matty.ds@alice.it>
    Co-authored-by: andrea-cerone <andreacerone97@yahoo.it>

--- Site/lib/js/navbar.js
+++ Site/lib/js/navbar.js
@@ -1,12 +1,15 @@
 function includeNavBar() {
-  fetch("/navbar.html")
+  return fetch("/navbar.html")
     .then(response => {
       return response.text()
     })
     .then(data => {
+      if (document.getElementById("mainPokefanNavBar") != null) {
+        return;
+      }
       var curr = document.body.innerHTML;
-      document.body.innerHTML = data + curr ;
-      if (document.cookie.indexOf("PHPSESSID=")!=-1) {
+      document.body.innerHTML = data + curr;
+      if (document.cookie.indexOf("PHPSESSID=") != -1) {
         // Im logged in
         $("#gobattle").show();
         $("#logout").show();
@@ -25,15 +28,15 @@ function includeNavBar() {
     });
 
 };
-function logout(){
+function logout() {
   document.cookie = "PHPSESSID=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
   document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
   location.reload();
 }
 
-function search(){
+function search() {
   var search = $("#navbar-search").val().toLowerCase();
-  window.location.href = "/search.php?s="+search;
+  window.location.href = "/search.php?s=" + search;
   return false;
 }
 
